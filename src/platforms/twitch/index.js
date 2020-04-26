e64b3343ec5770d3fe6f6117c98c925960847e3b
Eric Bishop <futureflyer.eric@gmail.com>
2020-04-26 15:45:13 -0600

    fix(Commands)
    + Fixed bot being able to call it's own commands

---
+++ src/platforms/twitch/index.js
@@ -0,0 +1,48 @@
+const tmi = require('tmi.js');
+const rumpus = require('@bscotch/rumpus-ce');
+const bot = require('../../bot/index');
+require('dotenv').config();
+
+const delegationToken = process.env.STREAMER_DELEGATION_KEY ? process.env.STREAMER_DELEGATION_KEY : '';
+const channel = process.env.TWITCH_CHANNEL;
+const streamer = process.env.STREAMER_USERNAME;
+const prefix = process.env.PREFIX ? process.env.PREFIX : '!';
+
+// Rumpus CE SDK Docs: https://github.com/bscotch/rumpus-ce
+
+const options = {
+  options: {
+    debug: true
+  },
+  connection: {
+    reconnect: true
+  },
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
+  },
+  channels: [channel]
+};
+
+const rce = new rumpus.RumpusCE(delegationToken);
+const client = tmi.Client(options);
+const shenanibot = new bot(client, rce, {
+  channel: channel,
+  streamer: streamer,
+  prefix: prefix
+});
+
+(async function main() {
+  // Connect bot to server
+  client.connect();
+
+  client.on('connected', (address, port) => {
+    client.action(channel, 'Bot Connected!');
+  });
+
+  client.on('chat', async (channel, user, message, self) => {
+    if (self) return;
+
+    shenanibot.command(message, user.username);
+  });
+})();
